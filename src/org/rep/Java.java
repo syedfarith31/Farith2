34fa7d2bf1d81df5860e34cf93a72919545a9bc6
Barith A <41798@hexaware.com>
2019-03-30 12:21:16 +0530

    Base class

---
+++ src/org/rep/Java.java
@@ -0,0 +1,17 @@
+package org.rep;
+
+
+public class Java {
+	public void empId(int id) {
+		System.out.println(id);}
+		public void empId(String dob) {
+			System.out.println(dob);}
+		public static void main(String[] args) {
+			Java r=new Java();
+			r.empId(123);
+			r.empId("12.09.2019");
+		}
+
+
+		}
+
